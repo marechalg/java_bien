d1ce81d6589ab2c313e92ec31ce89eeee03ec514
PHILIPPO <clement.philippo@etudiant.univ-rennes.fr>
2025-06-03 14:34:45 +0200

    Nouvelles classes

--- Cours.java
+++ Cours.java
@@ -1,3 +1,52 @@
+import java.util.Date;
+
 public class Cours {
-    private String test;
+    private String type;
+    private Date dateDebut;
+    private String duree;
+    private String lieu;
+    private String niveau;
+
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public Date getDateDebut() {
+        return dateDebut;
+    }
+    public void setDateDebut(Date dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+    public String getDuree() {
+        return duree;
+    }
+    public void setDuree(String duree) {
+        this.duree = duree;
+    }
+    public String getLieu() {
+        return lieu;
+    }
+    public void setLieu(String lieu) {
+        this.lieu = lieu;
+    }
+    public String getNiveau() {
+        return niveau;
+    }
+    public void setNiveau(String niveau) {
+        this.niveau = niveau;
+    }
+    public Cours(String type, Date dateDebut, String duree, String lieu, String niveau) {
+        this.type = type;
+        this.dateDebut = dateDebut;
+        this.duree = duree;
+        this.lieu = lieu;
+        this.niveau = niveau;
+    }
+    @Override
+    public String toString() {
+        return "Cours [type=" + type + ", dateDebut=" + dateDebut + ", duree=" + duree + ", lieu=" + lieu + ", niveau=" + niveau + "]";
+    }
+    
 }
