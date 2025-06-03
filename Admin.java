e15159bb3305629df85e68624eb97e7f168ed695
marechalg <guewenmarechal@protonmail.com>
2025-06-03 14:37:46 +0200

    pre build admin

--- Admin.java
+++ Admin.java
@@ -1,3 +1,21 @@
 public class Admin {
-    
+    private ArrayList<Professeur> professeurs;
+
+    public Admin() {
+        this.professeurs = new ArrayList<>();
+    }
+
+    public ajouterProfesseur(Professeur professur) throws Exception {
+        if (professeur != null) {
+            if (!this.professurs.contains(professeur)) {
+                this.professeurs.add(professur);
+            } else throw new Exception("Le professur existe déjà.");
+        } else throw new Exception("Le professur est invalide.");
+    }
+
+    public retirerProf(Professur professeur) throws Exception {
+        if (this.professeur.contains(professur)) {
+            
+        }
+    }
 }
