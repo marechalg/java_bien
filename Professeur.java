66d56b5552b4068c4b9cf166054d4abfe3aa2c38
marechalg <guewenmarechal@protonmail.com>
2025-06-03 14:40:16 +0200

    pre build prof

--- Professeur.java
+++ Professeur.java
@@ -1,3 +1,20 @@
+import java.util.Map;
+
 public class Professeur {
+    private String nom;
+    private String prenom;
+    private String specialite;
+    private Map<String, Integer> nbSeances;
+
+    public Professur(String nom, String prenom, String specialite) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.specialite = specialite;
+
+        this.nbSeances["passees"] = 0;
+        this.nbSeances["futures"] = 0;
+        this.nbSeances["total"] = this.nbSeances["futur"] + this.nbSeances["passees"];
+    }
+
     
 }
