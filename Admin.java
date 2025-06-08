54c56ab6c66faa9fa8d0ba5144f3e4adc9dbc421
marechalg <guewenmarechal@protonmail.com>
2025-06-08 18:46:08 +0200

    petits changements

--- Admin.java
+++ Admin.java
@@ -3,6 +3,10 @@ import java.util.ArrayList;
 public class Admin {
     static ArrayList<Professeur> professeurs = new ArrayList<>();
 
+    static ArrayList<Professeur> getProfesseurs() {
+        return Admin.professeurs;
+    }
+
     static void ajouterProfesseur(Professeur professeur) throws Exception {
         if (professeur != null) {
             if (!Admin.professeurs.contains(professeur)) {
@@ -13,28 +17,17 @@ public class Admin {
 
     static void retirerProf(Professeur professeur) throws Exception {
         if (Admin.professeurs.contains(professeur)) {
-            if (professeur != null){
-                Admin.professeurs.remove(professeur);
-                professeur = null;
-                System.gc();
-            } else throw new Exception("Le professeur est invalide.");
-        } else throw new Exception("Le professeur existe déjà.");
+            Admin.professeurs.remove(professeur);
+            professeur = null;
+            System.gc();
+        } else throw new Exception("Le professeur n'existe pas.");
     }
 
-    static void modifierProf(Professeur professeur, int index) throws Exception{
+    static void modifierProf(Professeur professeur, int index) throws Exception {
         if (index >= 0){
             if (Admin.professeurs.contains(professeur)){
                 Admin.professeurs.set(index, professeur);
             } else throw new Exception("Le professeur n'existe pas");
         } else throw new Exception("Erreur l'index ne peut pas être inférieur à 0.");
-        
-    }
-
-    static ArrayList<Professeur> getProfesseurs() {
-        return Admin.professeurs;
-    }
-
-    static void setProfesseurs(ArrayList<Professeur> professeurs) {
-        Admin.professeurs = professeurs;
     }
 }
