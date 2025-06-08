31fd40b9ad1b0f3d6c0e3a73707636a272218f55
marechalg <guewenmarechal@protonmail.com>
2025-06-08 19:00:25 +0200

    suppression de la variable d'instance de lieu (déplacé vers Seance.java) et suppression des accesseurs non nécessaires

--- Cours.java
+++ Cours.java
@@ -1,51 +1,20 @@
 public class Cours {
     private String type;
     private int duree;
-    private String lieu;
     private String niveau;
 
+    public Cours(String type, int duree, String niveau) {
+        this.type = type;
+        this.duree = duree;
+        this.niveau = niveau;
+    }
+
     public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public int getDuree() {
-        return duree;
-    }
-
-    public void setDuree(int duree) {
-        this.duree = duree;
-    }
-
-    public String getLieu() {
-        return lieu;
-    }
-
-    public void setLieu(String lieu) {
-        this.lieu = lieu;
-    }
-
-    public String getNiveau() {
-        return niveau;
-    }
-
-    public void setNiveau(String niveau) {
-        this.niveau = niveau;
-    }
-
-    public Cours(String type, int duree, String lieu, String niveau) {
-        this.type = type;
-        this.duree = duree;
-        this.lieu = lieu;
-        this.niveau = niveau;
+        return this.type;
     }
     
     @Override
     public String toString() {
-        return "Cours [type=" + type + ", duree=" + duree + ", lieu=" + lieu + ", niveau=" + niveau + "]";
+        return "Cours[type=" + this.type + ", duree=" + this.duree + ", niveau=" + this.niveau + "]";
     }
-    
 }
